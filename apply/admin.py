f5e3194a0b0568ec00a4b43a2b48557aa16ba19b
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2021-06-17 22:19:17 +0600

    Update admin.py

--- apply/admin.py
+++ apply/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ApplyModel
+from .models import ApplyModel,EmailModel
 # Register your models here.
 
 admin.site.register(ApplyModel)
+admin.site.register(EmailModel)
